2cfdf3625ba6bbfc9e26a2290d6c4b50ed63156e
zen <pierre.therrode@gmail.com>
2015-07-14 22:07:50 +0200

    correction

--- averagewordlength/src/main/java/org/apache/hadoop/averagewordlength/AverageReducer.java
+++ averagewordlength/src/main/java/org/apache/hadoop/averagewordlength/AverageReducer.java
@@ -7,15 +7,43 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class AverageReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+public class AverageReducer extends
+		Reducer<Text, IntWritable, Text, DoubleWritable> {
 
-  @Override
-  public void reduce(Text key, Iterable<IntWritable> values, Context context)
-      throws IOException, InterruptedException {
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
 
-    /*
-     * TODO implement
-     */
+		/*
+		 * TODO implement
+		 */
+		double sum = 0;
+		double count = 0;
 
-  }
+		/*
+		 * For each value in the set of values passed to us by the mapper:
+		 */
+		for (IntWritable value : values) {
+
+			/*
+			 * Add up the values and increment the count
+			 */
+			sum += value.get();
+			count++;
+		}
+		if (count != 0d) {
+
+			/*
+			 * The average length is the sum of the values divided by the count.
+			 */
+			double result = sum / count;
+
+			/*
+			 * Call the write method on the Context object to emit a key (the
+			 * words' starting letter) and a value (the average length per word
+			 * starting with this letter) from the reduce method.
+			 */
+			context.write(key, new DoubleWritable(result));
+		}
+	}
 }
--- averagewordlength/src/main/java/org/apache/hadoop/averagewordlength/LetterMapper.java
+++ averagewordlength/src/main/java/org/apache/hadoop/averagewordlength/LetterMapper.java
@@ -7,15 +7,43 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+/**
+ * 
+ * Type of input key = Object, Type of input value =Text, Type of output key=
+ * Text, Type of output value = IntWritable
+ * 
+ *
+ */
 public class LetterMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-  @Override
-  public void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
 
-    /*
-     * TODO implement
-     */
+	    String line = value.toString();
 
-  }
+	    /*
+	     * The line.split("\\W+") call uses regular expressions to split the
+	     * line up by non-word characters.
+	     * If you are not familiar with the use of regular expressions in
+	     * Java code, search the web for "Java Regex Tutorial."
+	     */
+	    for (String word : line.split("\\W+")) {
+	      if (word.length() > 0) {
+
+	        /*
+	         * Obtain the first letter of the word and convert it to lower case
+	         */
+	        String letter = word.substring(0, 1).toLowerCase();
+
+	        /*
+	         * Call the write method on the Context object to emit a key
+	         * and a value from the map method.  The key is the 
+	         * letter (in lower-case) that the word starts with; the value is the 
+	         * word's length.
+	         */
+	        context.write(new Text(letter), new IntWritable(word.length()));
+	      }
+	    }
+	}
+
 }
